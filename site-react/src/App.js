8ac63d471aac33d6f04ffa43d2a3b1543f448ee4
Jasper Yan <namti@aol.com>
2022-04-06 00:27:04 +1200

    Testing

--- site-react/src/App.js
+++ site-react/src/App.js
@@ -7,6 +7,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
+				<h5>Hello World! XD</h5>
 			</header>
 		</div>
 	);
