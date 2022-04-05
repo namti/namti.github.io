7c7a3bda3f56bdaef6d94e10a7c1d8285c9ca659
Jasper Yan <namti@aol.com>
2022-04-05 23:30:23 +1200

    Updates

---
+++ precache-manifest.ad4b27a0246979a8e1045b0ce94361a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c58c0c6e70acb5e2a43",
+    "url": "/site-react/static/css/main.045e6d4b.chunk.css"
+  },
+  {
+    "revision": "2c58c0c6e70acb5e2a43",
+    "url": "/site-react/static/js/main.2c58c0c6.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/site-react/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "c4d85cfae300d076fdc0",
+    "url": "/site-react/static/js/2.c4d85cfa.chunk.js"
+  },
+  {
+    "revision": "fe44516825d94792d893",
+    "url": "/site-react/static/js/runtime~main.fe445168.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/site-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "09b29df4435ca8effdd01292de9a96a9",
+    "url": "/site-react/index.html"
+  }
+];
