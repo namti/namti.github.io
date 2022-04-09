e42a9102c84ad372224169a528837048fed938af
Jasper Yan <namti@aol.com>
2022-04-09 16:16:53 +1200

    Install packages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h5>Hello World! XD</h5>
-			</header>
+		<div>
+			<Routes>
+				<Route path="/" element={ 
+					<header>
+						<h5>Hello World! XD</h5>
+					</header>
+					} />
+				<Route exact path="/about" element={ 
+					<h1>About</h1>
+					} />
+			</Routes>
 		</div>
 	);
 }
