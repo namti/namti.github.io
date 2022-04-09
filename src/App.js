70f8c031a0bdcb1e98caf4b0c7e55aabd8522f93
Jasper Yan <namti@aol.com>
2022-04-09 17:14:11 +1200

    Work on the router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import { 
+	Routes, 
+	Route, 
+	Link,
+	useLocation,
+} from "react-router-dom";
+import {
+	TransitionGroup,
+	CSSTransition
+} from "react-transition-group";
 
 function App() {
+	let location = useLocation();
+
 	return (
 		<div>
-			<Routes>
-				<Route path="/" element={ 
-					<header>
-						<h5>Hello World! XD</h5>
-					</header>
-					} />
-				<Route exact path="/about" element={ 
-					<h1>About</h1>
-					} />
-			</Routes>
+			<div>
+				<Link to="/">Home</Link>
+				<Link to="/about">About</Link>
+			</div>
+
+			<TransitionGroup>
+				<CSSTransition
+					key={ location.pathname }
+					classNames="fade"
+					timeout={3000}
+				>
+					<Routes>
+						<Route path="/" element={ 
+							<header>
+								<h5>Hello World! XD</h5>
+							</header>
+							} />
+						<Route exact path="/about" element={ 
+							<h1>About</h1>
+							} />
+					</Routes>
+				</CSSTransition>
+			</TransitionGroup>
+
 		</div>
 	);
 }
