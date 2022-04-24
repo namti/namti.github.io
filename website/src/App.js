81cc81c1fb64e43ef4f951fda7ab2cc781403dd7
Jasper Yan <namti@aol.com>
2022-04-24 18:36:44 +1200

    Add svg package

--- website/src/App.js
+++ website/src/App.js
@@ -7,14 +7,20 @@ import {
 import { Counter } from './features/counter/Counter';
 import { Works } from './features/work/works';
 import './view/dist/style.css';
+import { ReactSVG } from 'react-svg';
+import logo from './view/images/logo.svg';
 
 function App() {
 	return (
 		<Container>
 			<Row>
 				<Col>
+					<ReactSVG src={ logo } className="logo" />
+				</Col>
+			</Row>
+			<Row>
+				<Col>
 					<header>
-						<Counter />
 						<Works />
 					</header>
 				</Col>
